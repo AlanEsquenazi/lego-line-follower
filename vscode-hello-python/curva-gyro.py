fe02611fe6b86636a25a0e4834cb6b1e673e4a4c
botechat <vitorboechatbotelho@gmail.com>
2019-03-27 08:39:09 -0300

    Corrigido

--- vscode-hello-python/curva-gyro.py
+++ vscode-hello-python/curva-gyro.py
@@ -19,15 +19,15 @@ lm2.run_timed(speed_sp = 600, time_sp = 3000, stop_action = 'coast')
 sleep(1)
 
 #anda pra direita x ângulos
-gyro = ev3.GyroSensor('in1')
-a1 = gyro.angle()
+gyro = ev3.GyroSensor('in1','GYRO-ANG')
+a1 = gyro.value()
 a2 = 0
 while((a2-a1) < 90)
     #steer_pair.on_for_seconds(steering=100, speed=50, seconds=1)
     lm2.run_timed(speed_sp = 600, time_sp = 1, stop_action = 'coast')
     lm1.run_timed(speed_sp = -600, time_sp = 1, stop_action = 'coast')
     sleep(1)
-    a2 = gyro.angle()
+    a2 = gyro.value()
 
 #anda pra frente
 #steer_pair.on_for_seconds(steering=0, speed=50, seconds=3)
