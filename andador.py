327e5593f3775c21c858627cad60f2a564c5c88b
Alan <esquenazialan@gmail.com>
2019-04-22 15:23:55 -0300

    starts implementing calibration

--- andador.py
+++ andador.py
@@ -26,81 +26,42 @@ class Robot:
     def go_forward(self,speed,time):
         lm1.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
         lm2.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
-
-class LineFollower(Robot):
-    def __init__(self):
-        Robot.__init__(self)
-
-    def abrirAprendizadoWhite():
-    ## try: tenta abrir um arquivo de aprendizado
-    try:
-        with open("white_esq.txt", "r") as ft:            # a lista de aprendizado serah "azul, verde, vermelho"
-            white = ft.read().split(',')              # aqui, criamos uma lista de strings, cada elemento eh a cor
-            white.pop()
-            for x in white:
-                print("x", x, "int(x)", int(x))
-            white = [int(x) for x in white]     # tornamos as strings em inteiros
-    except:
-        white = [47, 47, 47]                         # caso nao haja arquivo, criamos a lista
-
-    def abrirAprendizadoBlack():
-    ## try: tenta abrir um arquivo de aprendizado
-    try:
-        with open("black_esq.txt", "r") as ft:            # a lista de aprendizado serah "azul, verde, vermelho"
-            black = ft.read().split(',')              # aqui, criamos uma lista de strings, cada elemento eh a cor
-            black.pop()
-            for x in black:
-                print("x", x, "int(x)", int(x))
-            black = [int(x) for x in black]     # tornamos as strings em inteiros
-    except:
-        black = [47, 47, 47]                         # caso nao haja arquivo, criamos a lista
-
-
-    def sensorEsquerdo():
-        for i in white:
-            for j in esq:
-                if (esq > white - 20 or esq < white + 20)
-                    return 0
-
-        for i in black:
-            for j in esq:
-                if (esq > black - 20 or esq < black + 20)
-                    return 1
-
-    def sensorDireito():
-        for i in white:
-            for j in dir:
-                if (dir > white - 20 or dir < white + 20)
-                    return 0
-
-        for i in black:
-            for j in dir:
-                if (dir > black - 20 or dir < black + 20)
-                    return 1
-
-
-    def follow(self):
-        #obs.: sem calibração, primeiro vamos testar com a leitura padrão do sensor
+    def receive_white(self):
+        with open("branco.txt", "r") as branco:
+            lims_branco = branco.read.split(",")
+        for i in range(6):
+            lims_branco[i] = int(lims_branco[i])
+    def receive_black(self):
+        with open("preto.txt", "r") as preto:
+            lims_preto = preto.read.split(",")
+        for i in range(6):
+            lims_preto[i] = int(lims_branco[i])
+    def follow_line(self):
         esq = se.raw()
-        esq = str(esq)
-        dir = sd.raw()
-        dir = str(dir)
-
+        direito = sd.raw()
+        if lims_branco[0]<=esq[0] and lims_branco[1]>=esq[0] and lims_branco[2]<=esq[1] and lims_branco[3]>=esq[1] and lims_branco[4]<=esq[2] and lims_branco[5]>=esq[2]:
+            esq = 0
+        elif lims_preto[0]<=esq[0] and lims_preto[1]>=esq[0] and lims_preto[2]<=esq[1] and lims_preto[3]>=esq[1] and lims_preto[4]<=esq[2] and lims_preto[5]>=esq[2]:
+            esq = 1
+        if lims_branco[0]<=direito[0] and lims_branco[1]>=direito[0] and lims_branco[2]<=direito[1] and lims_branco[3]>=direito[1] and lims_branco[4]<=direito[2] and lims_branco[5]>=direito[2]:
+            direito = 0
+        elif lims_preto[0]<=direito[0] and lims_preto[1]>=direito[0] and lims_preto[2]<=direito[1] and lims_preto[3]>=direito[1] and lims_preto[4]<=direito[2] and lims_preto[5]>=direito[2]:
+            direito = 1
         # 1 preto e 0 branco
         print(esq, " ", dir, '\n')
-        if(LineFollower.sensorEsquerdo() == 0 and LineFollower.sensorDireito() == 0): #white and white
+        if(esq == 0 and direito() == 0): #white and white
             Robot.go_forward(self, self.speed, 1)
 
-        if(LineFollower.sensorEsquerdo() == 1 and LineFollower.sensorDireito() == 1): #black and black
+        if(esq == 1 and direito == 1): #black and black
             sleep(1)
         else:
-            if(LineFollower.sensorEsquerdo() == 1 and LineFollower.sensorDireito() == 0): #black and white
-                while(not(LineFollower.sensorEsquerdo() == 0 and LineFollower.sensorDireito() == 0)):
+            if(esq == 1 and direito == 0): #black and white
+                while(not(esq == 0 and esq == 0)):
                     Robot.turn_left(self, self.speed, 1)
                     print(esq, " ", dir, '\n')
                     print("esquerda")
-            if(LineFollower.sensorEsquerdo() == 0 and LineFollower.sensorDireito() == 1): #white and black
-                while(not (LineFollower.sensorEsquerdo() == 0 and LineFollower.sensorDireito() == 0)):
+            if(esq == 0 and direito == 1): #white and black
+                while(not (esq == 0 and direito == 0)):
                     Robot.turn_right(self, self.speed, 1)
                     print("direita")
                     print(esq, " ", dir, '\n')
