de8b6011c41f88d6d3895622897b7497fb0745fb
Alan <esquenazialan@gmail.com>
2019-04-22 14:54:18 -0300

    calibration update

--- branco.py
+++ branco.py
@@ -9,7 +9,7 @@ lm1 = ev3.LargeMotor('outD'); assert lm1.connected
 lm2 = ev3.LargeMotor('outA'); assert lm2.connected
 
 cor = ev3.ColorSensor('in1'); assert cor.connected
-class Calibracao_andando:
+class Calibracao:
     def __init__(self, color, speed, time):
         self.color = color
         self.speed = speed
@@ -17,26 +17,11 @@ class Calibracao_andando:
         self.p1 = [1021,-1]
         self.p2 = [1021,-1]
         self.p3 = [1021,-1]
-    def calibrate(self, speed,time):
-        for i in range(100):
+    def calibrate(self, speed,time, wait_time, repeat):
+        for i in range(repeat):
             lm1.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
             lm2.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
-            cor_lida = cor.raw
-            self.p1[0] = min(cor.raw[0], self.p1[0])
-            self.p1[1] = max(cor.raw[0], self.p1[1])
-            self.p2[0] = min(cor.raw[1], self.p2[0])
-            self.p2[1] = max(cor.raw[1], self.p2[1])
-            self.p3[0] = min(cor.raw[2], self.p3[0])
-            self.p3[1] = max(cor.raw[2], self.p3[1])
-class Calibracao_parado:
-    def __init__(self, color):
-        self.color = color
-        self.p1 = [1021,-1]
-        self.p2 = [1021,-1]
-        self.p3 = [1021,-1]
-    def calibrate(self):
-        for i in range(10):
-            sleep(1000)
+            wait(wait_time)
             cor_lida = cor.raw
             self.p1[0] = min(cor.raw[0], self.p1[0])
             self.p1[1] = max(cor.raw[0], self.p1[1])
@@ -52,10 +37,10 @@ class Calibracao_parado:
             arquivo.write("\n") 
             arquivo.write(p3)
             arquivo.write("\n")  
-branco = Calibracao_andando("branco.txt", 400, 10)
-branco.calibrate(400, 10)
+branco = Calibracao("branco.txt", 400, 10)
+branco.calibrate(400, 10, 0, 100)
 branco.escrever()
 sleep(10000)
-preto = Calibracao_parado("preto.txt")
-preto.calibrate()
+preto = Calibracao_parado("preto.txt",0,0)
+preto.calibrate(0,0,2000, 10)
 preto.escrever()
