4330009a27616662bea054563beb627a4061aec5
Alan <esquenazialan@gmail.com>
2019-04-24 17:11:09 -0300

    Dias de Gloria

--- andador.py
+++ andador.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
 import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 from enum import Enum
@@ -17,13 +16,10 @@ class States(Enum):
 
 class Robot:
     def __init__(self,out1,out2,in1,in2):
-        self.speed = speed
-        self.time = time
-
-        self.self.lm1 = ev3.LargeMotor(self.out1); assert self.lm1.connected
-        self.self.lm2 = ev3.LargeMotor(self.out2); assert self.lm2.connected
-        self.se = ev3.ColorSensor(self.in1); assert self.se.connected
-        self.sd = ev3.ColorSensor(self.in2); assert self.sd.connected
+        self.lm1 = ev3.LargeMotor(out1); assert self.lm1.connected
+        self.lm2 = ev3.LargeMotor(out2); assert self.lm2.connected
+        self.se = ev3.ColorSensor(in1); assert self.se.connected
+        self.sd = ev3.ColorSensor(in2); assert self.sd.connected
 
     def turn_left(self,speed,time):
         self.lm1.run_timed(speed_sp = -speed, time_sp = time, stop_action = 'coast')
@@ -35,17 +31,17 @@ class Robot:
         self.lm1.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
         self.lm2.run_timed(speed_sp = -1*speed, time_sp = time, stop_action = 'coast')
     def abrirAprendizado(self,color,txt):
-        with open(self.txt, "r") as ft:            # a lista de aprendizado serah "azul, verde, vermelho"
-            self.color = ft.read().split(',')              # aqui, criamos uma lista de strings, cada elemento eh a cor
-            self.color.pop()
-            for x in self.color:
+        with open(txt, "r") as ft:            # a lista de aprendizado serah "azul, verde, vermelho"
+            color = ft.read().split(',')              # aqui, criamos uma lista de strings, cada elemento eh a cor
+            color.pop()
+            for x in color:
                 print("x", x, "int(x)", int(x))
-            self.color = [int(x) for x in self.color]     # tornamos as strings em inteiros
+            color = [int(x) for x in color]     # tornamos as strings em inteiros
 
     def verificaCor(self):
         # 1 preto e 0 branco
-        left = self.se.raw()
-        right = self.sd.raw()
+        left = self.se.raw
+        right = self.sd.raw
         if branco[0]<=left[0] and branco[1]>=left[0] and branco[2]<=left[1] and branco[3]>=left[1] and branco[4]<=left[2] and branco[5]>=left[2]:
             left = 0
         elif preto[0]<=left[0] and preto[1]>=left[0] and preto[2]<=left[1] and preto[3]>=left[1] and preto[4]<=left[2] and preto[5]>=left[2]:
@@ -56,19 +52,21 @@ class Robot:
             right = 1
 
     def verificaEstado(self):
-        if left == 1 and right == 0:
+        left = self.se.raw
+        right = self.sd.raw
+        if branco[0]<=left[0] and branco[1]>=left[0] and branco[2]<=left[1] and branco[3]>=left[1] and branco[4]<=left[2] and branco[5]>=left[2]:
             estado = States(-1)
-        if left == 0 and right == 0:
+        elif preto[0]<=left[0] and preto[1]>=left[0] and preto[2]<=left[1] and preto[3]>=left[1] and preto[4]<=left[2] and preto[5]>=left[2]:
             estado = States(0)
-        if left == 0 and right == 1:
+        if branco[0]<=right[0] and branco[1]>=right[0] and branco[2]<=right[1] and branco[3]>=right[1] and branco[4]<=right[2] and branco[5]>=right[2]:
             estado = States(1)
-        if left == 1 and right == 1:
+        elif preto[0]<=right[0] and preto[1]>=right[0] and preto[2]<=right[1] and preto[3]>=right[1] and preto[4]<=right[2] and preto[5]>=right[2]:
             estado = States(2)
 
     def follow_line(self,speed_reta,speed_curva):
         while(True):
-            Robot.verificaCor()
-            Robot.verificaEstado()
+            Robot.verificaCor(self)
+            Robot.verificaEstado(self)
             print(left, " ", right, " ", estado, '\n')
 
 
@@ -93,10 +91,11 @@ class Robot:
                     Robot.turn_right(self.speed_curva, 1)
                     print(left, " ", right, '\n')
                     print(States(1))
-
-Corsa = Robot('outA','outD','in2','in4')
-Corsa.abrirAprendizado(branco,branco.txt)
-Corsa.abrirAprendizado(preto,preto.txt)
+branco = [0,0,0,0,0,0]
+preto = [0,0,0,0,0,0]
+Corsa = Robot('outB','outD','in2','in4')
+Corsa.abrirAprendizado(branco,"branco.txt")
+Corsa.abrirAprendizado(preto,"preto.txt")
 Corsa.follow_line(400,800)
 #obs.: testar curva com duas rodas girando (uma muito pouco), com uma parada e outra girando ou com uma para cada lado
 
