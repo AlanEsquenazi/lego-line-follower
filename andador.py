eed34e5c8ad17ec0073d4ac7dcfd6115fe537794
botechat <vitorboechatbotelho@gmail.com>
2019-05-13 16:15:35 -0300

    todos os dias quando acordo

--- andador.py
+++ andador.py
@@ -22,7 +22,6 @@ class States(Enum):
 class Robot:
     def __init__(self,out1,out2,in1,in2,in3):
 
-
         self.lm1 = ev3.LargeMotor(out1); assert self.lm1.connected
         self.lm2 = ev3.LargeMotor(out2); assert self.lm2.connected
         self.se = ev3.ColorSensor(in1); assert self.se.connected
@@ -41,6 +40,10 @@ class Robot:
         self.lm1.run_timed(speed_sp = -speed, time_sp = time, stop_action = 'coast')
         self.lm2.run_timed(speed_sp = -speed, time_sp = time, stop_action = 'coast')
 
+    def stop(self,time):
+        self.lm1.run_timed(speed_sp = 0, time_sp = time, stop_action = 'coast')
+        self.lm2.run_timed(speed_sp = 0, time_sp = time, stop_action = 'coast')
+
     def curva_esquerda(self,speed,estado):
         self.lm1.run_timed(speed_sp = 0, time_sp = 500, stop_action = 'coast')
         self.lm2.run_timed(speed_sp = 0, time_sp = 500, stop_action = 'coast')
@@ -49,6 +52,7 @@ class Robot:
             Robot.verificaEstado(self)
             Robot.verificaVerde(self)
             Robot.turn_left(self,speed, 60)
+            Robot.stop(self,30)
             print(esquerdo, " ", meio, " ", direito, " ", estado)
 
     def curva_direita(self,speed,estado):
@@ -59,6 +63,7 @@ class Robot:
             Robot.verificaEstado(self)
             Robot.verificaVerde(self)
             Robot.turn_right(self,speed, 60)
+            Robot.stop(self,30)
             print(esquerdo, " ", meio, " ", direito, " ", estado)
 
     def meia_volta(self,speed, lendo_preto = 0, conta=0):
@@ -72,7 +77,7 @@ class Robot:
                     meia_volta(self,speed,0, 1)
             if meio==1 and conta==1:
                 meia_volta(self,speed,1,2)
-            
+
 
     def abrirAprendizadoBranco(self):
         global branco
