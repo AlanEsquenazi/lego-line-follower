14e8fa4b7f162b93b4921e7b541c0107a66cb1dc
botechat <vitorboechatbotelho@gmail.com>
2019-05-20 11:14:50 -0300

    curva como a da ROboime / today is gonna be the day

--- teste_curva.py
+++ teste_curva.py
@@ -7,23 +7,22 @@ from enum import Enum
 from time import sleep
 from time import time
 
-lm1 = ev3.LargeMotor('outB'); assert lm1.connected
-lm2 = ev3.Large4Motor('outD'); assert lm2.connected
+motorEsq = ev3.LargeMotor('outB'); assert motorEsq.connected
+motorDir = ev3.LargeMotor('outD'); assert motorDir.connected
 
 
-'''def stop(time):
+def stop(time):
     lm1.run_timed(speed_sp = 0, time_sp = time, stop_action = 'coast')
     lm2.run_timed(speed_sp = 0, time_sp = time, stop_action = 'coast')
 
-def curva_esquerda(speed,time):
-    while(True):
-        lm1.run_timed(speed_sp = speed, time_sp = time, stop_action = 'coast')
-        lm2.run_timed(speed_sp = 0, time_sp = time, stop_action = 'coast')
+def curva_esquerda(v_curva,pos_esq):
+        motorDir.run_to_rel_pos(position_sp = pos_esq, speed_sp = v_curva)
+        motorEsq.run_to_rel_pos(position_sp = - pos_esq, speed_sp = v_curva)
+        motorDir.wait_while("running")
+        motorEsq.wait_while("running")
 
 
-curva_esquerda(600,60)'''
+curva_esquerda(400,460)
 
-lm1.run_to_rel_pos(position_sp = 1000)
-lm2.run_to_rel_pos(position_sp = 1000)
 
 
