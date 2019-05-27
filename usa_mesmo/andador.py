7adf72e52e0d8240824f7cbc90d9f677492c7496
botechat <vitorboechatbotelho@gmail.com>
2019-05-27 08:04:07 -0300

    Não lê branco mais

--- usa_mesmo/andador.py
+++ usa_mesmo/andador.py
@@ -130,16 +130,26 @@ class Robot:
         middle = self.sm.raw
         if preto_meio[0]<=middle[0] and preto_meio[1]>=middle[0] and preto_meio[2]<=middle[1] and preto_meio[3]>=middle[1] and preto_meio[4]<=middle[2] and preto_meio[5]>=middle[2]:
             meio = 1
-        elif preto_meio[0]<=middle[0] and preto_meio[1]>=middle[0] and preto_meio[2]<=middle[1] and preto_meio[3]>=middle[1] and preto_meio[4]<=middle[2] and preto_meio[5]>=middle[2]:
+
+        else:
             meio = 0
+
 
         if preto[0]<=left[0] and preto[1]>=left[0] and preto[2]<=left[1] and preto[3]>=left[1] and preto[4]<=left[2] and preto[5]>=left[2]:
             esquerdo = 1
+
+        elif verde[0]<=left[0] and verde[1]>=left[0] and verde[2]<=left[1] and verde[3]>=left[1] and verde[4]<=left[2] and verde[5]>=left[2]:
+            esquerdo = 2
+
         else:
             esquerdo = 0
 
         if preto_direito[0]<=right[0] and preto_direito[1]>=right[0] and preto_direito[2]<=right[1] and preto_direito[3]>=right[1] and preto_direito[4]<=right[2] and preto_direito[5]>=right[2]:
             direito = 1
+
+        elif verde_direito[0]<=left[0] and verde_direito[1]>=left[0] and verde_direito[2]<=left[1] and verde_direito[3]>=left[1] and verde_direito[4]<=left[2] and verde_direito[5]>=left[2]:
+            direito = 2
+
         else:
             direito = 0
 
